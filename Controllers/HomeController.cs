abc472f6565cdf3fab2e04cb863ad53c417d62b6
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-05-25 16:38:47 +0900

    add

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,26 +6,17 @@ namespace MvcMovie.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
+  //
+    // GET: /HelloWorld/
+      public IActionResult Index()
     {
         return View();
     }
-
-    public IActionResult Privacy()
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
